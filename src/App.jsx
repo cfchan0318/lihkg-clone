2f4701f87ab7ee91b75a97a12319ac28d12a22ea
Marco Chan <cf.chan0318@gmail.com>
2022-06-04 11:19:01 +0800

    post styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,39 @@ import React from "react";
 import Post from "./components/posts";
 
 function App() {
+  const postData = [
+    {
+      "user": "Evelyn Cantu",
+      "title": "dolor, nonummy ac, feugiat non, lobortis quis, pede. Suspendisse dui. Fusce diam nunc, ullamcorper eu, euismod ac, fermentum vel, mauris.",
+      "pages": 3
+    },
+    {
+      "user": "Tucker Gardner",
+      "title": "consectetuer euismod est arcu ac orci. Ut semper pretium neque. Morbi quis urna. Nunc quis arcu vel quam dignissim pharetra.",
+      "pages": 8
+    },
+    {
+      "user": "Vielka Dennis",
+      "title": "libero nec ligula consectetuer rhoncus. Nullam velit dui, semper et, lacinia vitae, sodales at, velit. Pellentesque ultricies dignissim lacus. Aliquam",
+      "pages": 8
+    },
+    {
+      "user": "Doris Brown",
+      "title": "auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est, vitae sodales nisi magna sed dui.",
+      "pages": 3
+    },
+    {
+      "user": "Drew Stevens",
+      "title": "ut, sem. Nulla interdum. Curabitur dictum. Phasellus in felis. Nulla tempor augue ac ipsum. Phasellus vitae mauris sit amet lorem",
+      "pages": 5
+    }
+  ];
   return (
     <div className="App">
-      <Post></Post>
+      {postData.map(post => (
+        <Post user={post.user} title={post.title} pages={post.pages}/>
+      ))}
+      
     </div>
   );
 }
--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,47 +1,52 @@
 import styled from 'styled-components'
 import React from 'react'
 
-const Element = ({ className }) => {
+const Element = ({ className,id,user,title,pages }) => {
     return (
         <div className={className}>
             <div className='post-header'>
                 <div className='post-info'>
-                    <div className='user'>
-                        <span>cfchan0318</span>
-                    </div>
+                    <div className='post-user'>{user}</div>
+                   
                 </div>
-                <div className='post-pages'>1頁</div>
+                <div className='post-pages'>{pages}頁</div>
             </div>
             <div className='post-content'>
-                <span>有冇人有自己嘅side project?</span>
+                <span className='post-title'>{title}</span>
             </div>
         </div>
     );
 }
 
 const Post = styled(Element)`
-  height: 100px;
+  min-height: 100px;
   width: 400px;
   border-radius: 10px;
-  border: 1px solid black;
-  padding: 10px;
+  background-color:#16161a;
+  padding: 30px;
+  margin-bottom:10px;
 
   .post-header{
       display:flex;
-      margin-bottom:20px;
+      margin-bottom:10px;
   }
   .post-info{
       flex:10;
       display:flex;
   }
+  .post-user{
+      color:#fffffe;
+  }
   .post-pages{
       flex:2;
       display:flex;
       justify-content: flex-end;
+      color: #7f5af0;
   }
 
   .post-content>span{
-      font-size: 18px;
+      font-size: 24px;
+      color:#94a1b2;
   }
 
 `
