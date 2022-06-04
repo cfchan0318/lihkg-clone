6ddfd4862968e371d93638ba58b5d50cb65a257c
Marco Chan <cf.chan0318@gmail.com>
2022-06-04 23:56:26 +0800

    Post List Header layout

--- src/components/post-list-header/index.jsx
+++ src/components/post-list-header/index.jsx
@@ -5,10 +5,13 @@ const PostListHeader = ({ className }) => {
   return (
     <div className={className}>
       <div class="post-list-header-left">
-        <i onClick={() => { alert(); }} class="fi fi-rr-menu-burger" />   
+        <i onClick={() => { console.log("open")}} class="fi fi-rr-menu-burger" />   
       </div>
       <div class="post-list-header-center">React</div>
-      <div class="post-list-header-right"></div>
+      <div class="post-list-header-right">
+        <i class="fi fi-rr-refresh" />
+        <i class="fi fi-rr-add"></i>
+      </div>
     </div>
   )
 }
@@ -35,7 +38,14 @@ const StyledPostListHeader = styled(PostListHeader)`
   }
   .post-list-header-right {
     flex: 1;
+    color:white;
     padding-right:10px;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .fi-rr-refresh{
+    margin-right: 20px;
   }
 `
 export default StyledPostListHeader
