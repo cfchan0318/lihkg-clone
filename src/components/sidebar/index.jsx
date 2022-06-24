78bc3d8fc9bd69ba5307eeac87885f5485fa60ec
Chi Fung Chan <cf.chan0318@gmail.com>
2022-06-24 12:35:30 +0800

    sidebar layout fix

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -9,27 +9,29 @@ const Sidebar = ({ className, isOpen, closeOnClick }) => {
   const dispatch = useDispatch()
 
   React.useEffect(() => {
-    dispatch(fetchChannels());
+    dispatch(fetchChannels())
   }, [dispatch])
 
   return (
     <div className={className}>
-      <div className="sidebar-header">
-        <StyledCustomButton
-          onClick={() => {
-            closeOnClick()
-          }}
-        >
-          <i className="fi fi-rr-cross-small" />
-        </StyledCustomButton>
-        
-      </div>
-      <div className="sidebar-channels">
-        {channels.map(channel => (
-          <div className="sidebar-channel" key={channel.id}>{channel.name}</div>
-        ))}
-        
+      <div className="sidebar-content">
+        <div className="sidebar-header">
+          <StyledCustomButton
+            onClick={() => {
+              closeOnClick()
+            }}
+          >
+            <i className="fi fi-rr-cross-small" />
+          </StyledCustomButton>
         </div>
+        <div className="sidebar-channels">
+          {channels.map((channel) => (
+            <div className="sidebar-channel" key={channel.id}>
+              {channel.name}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
@@ -38,33 +40,44 @@ const StyledSideBar = Styled(Sidebar)`
     z-index:1;
     position:fixed;
     top:0;
-    left:-60px;
-    overflow-x:hidden;
-    padding:30px;
+    padding-top:30px;
     height: 100vh;
     width: ${({ isOpen }) => (isOpen ? '320px' : '0px')};
     background-color:#242629;
 
-    transition: all 0.5s;
-    -webkit-transition: all 0.5s;
+    transition: all 0.5s ease;
+    -webkit-transition: all 0.5s ease;
+
+    .sidebar-content{
+     padding-left:30px;
+     padding-right:30px;
+    }
     
     .sidebar-header{
       width:100%;
+      min-width:0px;
       display:flex;
-     
-     justify-content:flex-end;
+      justify-content:flex-end;
+      flex-wrap:no-wrap;
+      overflow:hidden;
+
     }
 
     .sidebar-channels{
       width:100%;
       display:flex;
-     
-     justify-content:flex-end;
+      flex-wrap: wrap;
     }
 
     .sidebar-channel{
-      background-color:white;
-      color:black;
+      min-width:50%;
+      max-width:50%;
+      white-space: nowrap;
+      word-break:break-word;
+      overflow:hidden;
+      display:flex;
+      color:#fffffe;
+      justify-content:center;
     }
 
     .fi{
